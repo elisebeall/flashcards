73198180090ef0bb82f5e55c9e49979b7bf3aebf
Elise Beall <elisebeall@gmail.com>
2021-08-27 14:43:06 -0600

    Complete Round class

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,13 +6,13 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe ('Round', function() {
-  it ('should be an object', function() {
+  it ('should be a function', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
-    assert.typeOf(round, 'object');
+    assert.typeOf(Round, 'function');
   });
   it ('should have a deck of cards', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
@@ -20,7 +20,7 @@ describe ('Round', function() {
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
-    assert.deepEqual(round.deck, [card1, card2, card3]);
+    assert.instanceOf(round, Round);
   });
   it ('should be able to return the current card', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
@@ -56,7 +56,7 @@ describe ('Round', function() {
     round.takeTurn('spleen');
     assert.deepEqual(round.turns, 2);
   });
-  it ('should keep track of the incorrect guesses', function() {
+  it ('should keep track of IDs of the incorrect guesses', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
@@ -64,16 +64,26 @@ describe ('Round', function() {
     const round = new Round(deck);
     round.takeTurn('sea otter');
     round.takeTurn('spleen');
-    assert.deepEqual(round.incorrectGuesses[0], [card2]);
+    assert.deepEqual(round.incorrectGuesses[0], card2.id);
   });
-  it ('should calculate the percentage of correct guesses', function() {
+  it ('should calculate the percentage of correct guesses out of total guesses', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     round.takeTurn('sea otter');
-    round.takeTurn('spleen');
+    round.takeTurn('capybara');
+    const percentCorrect = round.calculatePercentCorrect();
     assert.deepEqual(round.calculatePercentCorrect(), 50);
-  })
+  });
+  it ('should give feedback after each turn', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    const feedback = round.takeTurn('sea otter');
+    assert.deepEqual(feedback, 'correct!');
+  });
 });
