70360d95754f0bd99a871d3512c549d1ef217a1c
Elise Beall <elisebeall@gmail.com>
2021-08-27 15:26:24 -0600

    Start game setup

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,23 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.round = "";
+  }
+
+  start() {
+    let cards = [];
+    cards.forEach(card, function() {
+      cards.push(card);
+    });
+    let deck = new Deck(cards);
+    this.round = new Round(deck);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
